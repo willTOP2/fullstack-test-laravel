a860612444524955caca2ff17acaaae895337a3e
Cristiano Fromagio <cristianofromagio@gmail.com>
2021-05-11 18:59:49 -0300

    Start

---
+++ README.md
@@ -0,0 +1,21 @@
+# Fullstack Test Laravel
+
+## Descrição
+
+Projeto em Laravel (v8.x) criado com o comando `composer create-project laravel/laravel fullstack-test-laravel`.
+
+## Pré-requisitos e ambiente de desenvolvimento
+
+- PHP >= 7.3 (sugestão: se estiver utilizando Windows, use o [Laragon](https://laragon.org/download/) e siga [este tutorial](https://forum.laragon.org/topic/166/tutorial-how-to-add-another-php-version) para adicionar novas versões do PHP)
+- [Composer](https://getcomposer.org/) 2.0 package manager
+
+## Referências
+
+- [Documentação oficial](https://laravel.com/docs/8.x) do Laravel
+- [Coleção de links](https://raindrop.io/fromagio-cristiano/penze-laravel-5191096) selecionados e organizados no Raindrop
+
+## Comandos úteis
+
+- `php artisan serve`: iniciar servidor local de desenvolvimento em `http://localhost:8000/`
+- `php artisan make:foobar FoobarName`: gerar classes e componentes
+- `php artisan list`: ver todos os comandos do `artisan`
---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HelloController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'mensagem' => 'Olá mundo, servidor api funcionando!'
+    ]);
+});
+
+Route::get('/hello', [ HelloController::class, 'hello' ]);
+
+// TODO: criar uma rota que retorne a data-hora atual (sugestão: use a função `now()`)
+// TODO: criar uma rota que chame uma função em um controller e retorne o texto "Laravel + Angular"
